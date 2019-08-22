ff7c4654e1a6cf60618467e68016c6719d019ba5
Niqui O'Neill <doneill@ncsu.edu>
2019-08-22 15:57:52 -0400

    add in desc/asc

--- jekyll-lunr-js-custom-search.gemspec
+++ jekyll-lunr-js-custom-search.gemspec
@@ -19,5 +19,5 @@ Gem::Specification.new do |s|
   s.add_runtime_dependency 'therubyracer', '~> 0.12.3'
 
   s.add_development_dependency 'rake', '~> 12.0'
-  s.add_development_dependency 'jsmin', '~> 1.0.1'
+  s.add_development_dependency 'jsmin', '<= 1.0.1'
 end
--- js/lunr.custom.search.js
+++ js/lunr.custom.search.js
@@ -63,11 +63,20 @@ function createSearch(values, origsearch_dict, sort, lunr_settings){
     all_results[results[i].ref] = dictionary
   }
 
-  if (sort){
-  	var sort_field = sort == 'atoz' ? lunr_settings['atozsortfield'] : sort;
+ if (sort){
+    var ascdesc = 'asc'
+    if (sort.split('___').length > 1){
+      var split = sort.split('___');
+      sort = split[0]
+      ascdesc = split[1]
+    }   
+    var sort_field = sort == 'atoz' ? lunr_settings['atozsortfield'] : sort;
     var sorted = _.sortBy(Object.values(all_results), function(item) {
      return [String(item[sort_field]).normalize('NFD'), String(item[lunr_settings['atozsortfield']]).normalize('NFD')];
     })
+    if (ascdesc == 'desc'){
+      sorted = sorted.reverse();
+    }
     var sorted_dict = {}
     for (var j=0; j<sorted.length; j++){
       sorted_dict[sorted[j]['slug']] = sorted[j]
