cab5b74da5b5145b6a1f17543bd41ce2eebcbb8b
Niqui <doneill@ncsu.edu>
2022-08-05 19:46:25 -0400

    update build

--- docs/assets/js/just-the-docs.js
+++ docs/assets/js/just-the-docs.js
@@ -44,7 +44,7 @@ function initNav() {
 
 function initSearch() {
   var request = new XMLHttpRequest();
-  request.open('GET', 'http://localhost:4000/jekyll-lunr-js-custom-search/assets/js/search-data.json', true);
+  request.open('GET', 'https://dnoneill.github.io/jekyll-lunr-js-custom-search/assets/js/search-data.json', true);
 
   request.onload = function(){
     if (request.status >= 200 && request.status < 400) {
